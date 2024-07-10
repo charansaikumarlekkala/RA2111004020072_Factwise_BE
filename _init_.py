e866202169ab530235f694b755936cd650836729
charansaikumarlekkala <cl1556@srmist.edu.in>
2024-07-10 12:51:52 +0530

    _init_.py

--- _init_.py
+++ _init_.py
@@ -1,30 +1,14 @@
-import json
-import os
-from datetime import datetime
-from project_board_base import
-ProjectBoardBase
+from concrete.user_impl import UserImpl
+from concrete.team_impl import TeamImpl
+from concrete.board_impl import BoardImpl
+from concrete.task_impl import TaskImpl
 
-DB_PATH='db/'
-
-class 
-ProjectBoard(ProjectBoardBase):
-          def_init_(self):
-            if not
-       os.path.exists(DB_PATH):
-             os.makedirs(DB_PATH)
-          def_load_data(set,filename):
-            filepath=os.path.join(DB_PATH)
-              if os.path.exists(filepath):
-                 with open(filepath,'r')as
-       file:
-                   return json.load(file)
-             return{}
-
-          def_save_data(self,filename,data):
-            filepath=os.path.join(DB_PATH,filename)
-                  with open(filepath,'w')as file:
-                    json.dump(date,file,indent=4)
-          def create_board(self,request:str)->str:
-            data=json.loads(request)
-            team_id=data['team_id']
-            boards=self._load_data('board.json')
+def get_all_users():
+          return UserImpl.get_all()
+def get_all_teams():
+          return TeamImpl.get_all()
+def get_all_boards():
+          return BoardImpl.get_all()
+def get_all_tasks():
+          return TaskImpl.get_all()
+                                        
